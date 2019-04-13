a9a26152dc96abfaa610faf626d58ee80462eb22
Malachi Irwin <malachi@theirwins.ws>
2019-04-13 07:21:48 -0600

    FIxed pdf

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,8 +12,8 @@ class StaticPagesController < ApplicationController
   end
 
   def trick_list
-    pdf_filename = File.join(Rails.root, "tmp/MWRLadder.pdf")
-    send_file(pdf_filename, :filename => "your_document.pdf", :disposition => 'inline', :type => "application/pdf")
+    @pdf_filename = File.join(Rails.root, "tmp/MWRLadder.pdf")
+    send_file(@pdf_filename, :filename => "MWRLadder.pdf", :disposition => 'inline', :type => "application/html")
   end
 
   def resources
