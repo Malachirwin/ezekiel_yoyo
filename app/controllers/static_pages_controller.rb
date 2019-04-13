d4f8486496271377a799fa38e58c9b80d5629e93
Malachi Irwin <malachi@theirwins.ws>
2019-04-13 07:32:20 -0600

    CHanged the pdf to a image

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,8 +12,6 @@ class StaticPagesController < ApplicationController
   end
 
   def trick_list
-    @pdf_filename = File.join(Rails.root, "tmp/MWRLadder.pdf")
-    send_file(@pdf_filename, :filename => "MWRLadder.pdf", :disposition => 'inline', :type => "application/html")
   end
 
   def resources
